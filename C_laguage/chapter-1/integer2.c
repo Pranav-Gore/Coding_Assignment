21402709a6b34275f6e4c3b80e5b26b76f06d102
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-02-18 19:12:40 +0530

    Add files via upload

---
+++ C_laguage/chapter-1/integer2.c
@@ -0,0 +1,15 @@
+// Take a two integer and print it
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    printf("Enter the First number\n");
+    scanf("%d",&num1);
+
+    printf("Enter the Second Number\n");
+    scanf("%d",&num2);
+
+    printf("The First Number is:%d\n",num1);
+    printf("The Second Number is:%d\n",num2);
+
+    return 0;
+}
