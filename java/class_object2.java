921996ef2aa058ae5fb0e1eb224ec178e5305aa0
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-07-01 13:21:43 +0530

    Update class_object2.java

--- java/class_object2.java
+++ java/class_object2.java
@@ -1,1 +1,24 @@
+package helloworld.javaprograms;
+
+class demo2{
+	
+	int iNo;
+	
+	demo2(){	//constructor
+		iNo=30;
+	}
+	public void fun1() {
+		System.out.printf("value of iNo = %d\n",iNo);
+	    System.out.println("Inside Demo Class and inside fun function");
+	}
+}
+
+public class classobject2 {
+	
+	public static void main(String args[]) {
+		
+		demo2 obj2 = new demo2();
+		obj2.fun1();
+	}
+}
 
