e822366b1d90ec962ac76ecc02d12aaade946877
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-07-01 13:23:02 +0530

    Update class_object5.java

--- java/class_object5.java
+++ java/class_object5.java
@@ -1,1 +1,24 @@
+package helloworld.javaprograms;
+
+class details{
+	String name = "Pranav";
+	int age = 20;
+	String address = "Rui-chhatrapati";
+	
+	public void display() {
+		System.out.println("My name is "+name);
+		System.out.println("My age is "+age);
+		System.out.println("My address is "+address);
+	}
+}
+
+public class print_class_object5 {
+	
+	public static void main(String args[]) {
+		
+		details obj5 = new details();
+		obj5.display();
+	}
+
+}
 
