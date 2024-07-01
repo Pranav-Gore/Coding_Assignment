010199f26c350d8670279089a384c0adf011947a
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-07-01 13:22:36 +0530

    Update class_object4.java

--- java/class_object4.java
+++ java/class_object4.java
@@ -1,1 +1,19 @@
+package helloworld.javaprograms;
+
+class add{
+	int iNo1=10;
+	int iNo2=20;
+	
+	public void sum() {
+		System.out.println("Sum is "+(iNo1+iNo2));
+	}
+}
+
+public class print_class_object4 {
+
+	public static void main(String args[]) {
+		add obj4 = new add();
+		obj4.sum();
+	}
+}
 
