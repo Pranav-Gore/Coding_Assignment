edbdb9ba3258ea5b516d60871d72ac38740c6aec
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-07-01 13:22:09 +0530

    Update class_object3.java

--- java/class_object3.java
+++ java/class_object3.java
@@ -1,1 +1,19 @@
+package helloworld.javaprograms;
+
+class demo3{
+	int a=10;
+	String b="Pranav";
+	
+	void show() {
+		System.out.println(a+" "+b);
+	}
+}
+
+public class classobject3 {
+
+	public static void main(String args[]) {
+		demo3 obj3 = new demo3();
+		obj3.show();
+	}
+}
 
