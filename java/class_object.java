935128f63a6a7117dec0db0850bc244513de07fd
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-07-01 13:18:49 +0530

    Create class_object.java

---
+++ java/class_object.java
@@ -0,0 +1,17 @@
+package helloworld.javaprograms;
+
+class demo1{
+	
+	public void fun() {
+		System.out.println("In the demo1 class function");
+	}
+}
+
+public class classandobject {
+	
+	public static void main(String args[]) {
+		
+		demo1 obj1 = new demo1();
+		obj1.fun();
+	}
+}
