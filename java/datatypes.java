673cbeddcb6651dcd5d491e9edc6b40c621368e7
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-07-01 13:18:01 +0530

    Create datatypes.java

---
+++ java/datatypes.java
@@ -0,0 +1,30 @@
+package helloworld.javaprograms;
+
+public class myfirstprogram {
+
+	    public static void main(String[] args) {
+	        // Primitive data types
+	        int anInt = 10;
+	        double aDouble = 20.5;
+	        char aChar = 'A';
+	        boolean aBoolean = true;
+
+	        // Wrapper classes
+	        Integer integerObject = 100;
+	        Double doubleObject = 200.5;
+	        Character charObject = 'B';
+	        Boolean booleanObject = false;
+
+	        // Displaying the values
+	        System.out.println("int: " + anInt);
+	        System.out.println("double: " + aDouble);
+	        System.out.println("char: " + aChar);
+	        System.out.println("boolean: " + aBoolean);
+
+	        System.out.println("Integer object: " + integerObject);
+	        System.out.println("Double object: " + doubleObject);
+	        System.out.println("Character object: " + charObject);
+	        System.out.println("Boolean object: " + booleanObject);
+	    }
+	
+}
