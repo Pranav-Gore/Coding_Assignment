798ef5f32d4077605ac25da23e307ea1c167d86b
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-09-22 15:08:25 +0530

    Create IncrementDecrementExample2.java

---
+++ java/operators/IncrementDecrementExample2.java
@@ -0,0 +1,19 @@
+package com.example.myjavaprograms.operator;
+
+public class IncrementDecrementExample2 {
+    public static void main(String[] args) {
+        // Example 1: Pre-decrement and Post-decrement
+        int x = 1;
+        System.out.println(x);   
+        System.out.println(--x); 
+        System.out.println(x--); 
+        System.out.println(x);   
+
+        // Example 2: Another set with different initial value
+        x = 2;
+        System.out.println(x);   
+        System.out.println(x--); 
+        System.out.println(x);   
+    }
+}
+
