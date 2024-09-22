d243f009bf916e3e82b385e92cf6c51bb2e5abeb
Pranav Gore <143307944+Pranav-Gore@users.noreply.github.com>
2024-09-22 15:06:29 +0530

    Create ArithmeticOperations.java

---
+++ java/operators/ArithmeticOperations.java
@@ -0,0 +1,29 @@
+ackage com.example.myjavaprograms.operator;
+
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        // Example values
+        int num1 = 20;
+        int num2 = 10;
+        
+        // Addition
+        int additionResult = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + additionResult);
+        
+        // Subtraction
+        int subtractionResult = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + subtractionResult);
+        
+        // Multiplication
+        int multiplicationResult = num1 * num2;
+        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + multiplicationResult);
+        
+        // Division
+        int divisionResult = num1 / num2;
+        System.out.println("Division: " + num1 + " / " + num2 + " = " + divisionResult);
+        
+        // Modulus
+        int modulusResult = num1 % num2;
+        System.out.println("Modulus: " + num1 + " % " + num2 + " = " + modulusResult);
+  
